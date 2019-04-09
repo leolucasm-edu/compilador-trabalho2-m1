c6276cb3617be6cadf31db082846774044bccf4f
Leonardo Melo <leolucasm@edu.univali.br>
2019-04-09 01:35:40 -0300

    Trabalho

---
+++ src/compiladorv1/CompiladorV1.java
@@ -0,0 +1,15 @@
+package compiladorv1;
+
+import views.Principal;
+
+public class CompiladorV1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Principal principal = new Principal();
+        principal.setVisible(true);
+    }
+    
+}
